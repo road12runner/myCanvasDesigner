1e2a9ffda7db3a0062119959d6cdf392274677ab
dmitry <dmitry.panferov@gmail.com>
2017-09-08 17:04:00 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: "./entry.js",
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: "style!css" }
+    ]
+  }
+};
